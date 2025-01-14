951e32b211258a94e231c6ba4ed50029caeaade0
Ido Roth <idoroth4@gmail.com>
2025-01-14 21:23:12 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React Portfolio 2/",
+  base: "/React-Portfolio-2/",
   build: {
     outDir: "build",
   },
